b846ae85d0a4079c0875a3dbe9ddd63e83d2ff0d
sharky101 <dan.howitt@iondrivesoftware.com>
2015-08-10 17:24:44 +0100

    feat(base64): define export files with json object
    Kubernetes secrets are key value pairs, where the filename becomes the
    key and the value the file contents. In order to create an environment
    file and make it easy for editing, the iondrive cli will interpret any
    secret key starting with "env" and it's value being a json object as
    configuration to create an environment file.

--- src/secrets/Base64Encoder.ts
+++ src/secrets/Base64Encoder.ts
@@ -20,7 +20,31 @@ class Encoder {
       Object.keys(item.data).forEach(function (key) {
         var val = item.data[key];
         var isBase64 = base64Matcher.test(val);
+        
+        if (key === 'env') {
+          if (_.isObject(val)) {
+            var envString = '';
+            Object.keys(val).forEach(function (key) {
+              envString += `export ${key}=\"${val[key]}\"\n`;
+            });
+            val = envString;
+          }
+        }
+
         var decoded = new Buffer(val, isBase64 ? 'base64' : null).toString(isBase64 ? 'utf8' : 'base64');
+
+        if (key === 'env' && isBase64) {
+          var envObj: any = {};
+          decoded.split('\n').forEach(function (line) {
+            var keyVal = line.split('=');
+            if (keyVal.length > 1) {
+              var key = keyVal[0].replace('export ', '');
+              envObj[key] = keyVal[1].replace(/['"]+/g, '');
+            }
+          });
+          decoded = envObj;
+        }
+
         item.data[key] = decoded;
       })
     });
